9bf38fbfa8102509b62c04a371aaddf889874082
Igor <idontsov@seznam.cz>
2024-10-26 23:40:00 +0200

    Initial commit

--- server.py
+++ server.py
@@ -6,20 +6,112 @@ app = Flask(__name__)
 
 # HTML-шаблон для отображения формы и результатов
 html_template = '''
-    <form method="POST">
-        <label for="text">Введите текст с EAN кодами и количеством:</label><br>
+<style>
+        .motto {
+        display: flex;
+        
+        height: 12px;
+        flex-direction: column;
+        justify-content: center;
+        flex-shrink: 0;
+        margin-top: 9px;
+        color: #fff;
+        font-family: "arial", "serif";
+        font-size: 10.5px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: 24px;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        width: max-content;
+        text-decoration: none;
+            }
+        .table-container { 
+            display: flex; 
+            justify-content: left; 
+            gap: 10px; 
+            background-color: rgb(231, 227, 227);
+            margin-left: 5%;
+            padding: 3%;
+            width: max-content;
+        }
+        .top-menu-logo {
+            margin-left: 5%;
+        }
+        .top-menu-sale {
+        background: #457a0f;
+        display: flex;
+        height: 60px;
+        color: aliceblue;
+        padding: 1%;
+        position: relative;
+        
+        }
+        .form {
+        margin-left: 5%;
+        color: #10185a;
+        background-color: rgb(231, 227, 227);
+        padding: 1%;
+        max-width: fit-content;
+        }
+        .text {
+            margin-left: 5%;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        }
+        .button {
+            background-color: #457a0f;
+            color: #ffffff;
+            width: 30%;
+        }
+        footer {
+          text-align: center;
+          margin-top: 2%;
+        }
+        table {
+            background-color: #fff;
+            padding: 1%;
+            width: 100%;
+        }
+    </style>
+    
+    <div class ="top-menu-sale"> 
+        <div class="top-menu-logo">
+            <a href="https://www.lingea.cz" title="lingea" class="top-logo">
+            <img src="https://www.lingea.cz/img/lingea/logo-white.svg?v=1" title="lingea" alt="lingea">
+            
+            </a>
+            <span class="motto">Radost z poznávání</span>
+        </div>
+      </div>
+<span class="motto">Radost z poznávání</span>
+    
+    <label for="text" ><h2 class ="text">Enter text with EAN codes and quantity:</h2></label>
+    <form method="POST" class ="form">
+      
         <textarea id="text" name="text" rows="4" cols="50">{{ request.form.get('text', '') }}</textarea><br><br>
-        <input type="submit" value="Отправить">
+        <input type="submit" value="Submit" class="button">
     </form>
 
     {% if results %}
-        <h2>Результаты:</h2>
+        <h2 class ="text">Result:</h2>
+        <div class="table-container"> 
         <table border="1">
-            <tr><th>EAN Code</th><th>Quantity</th></tr>
+            <tr><th>EAN Code</th></tr>
             {% for ean, qty in results %}
-                <tr><td>{{ ean }}</td><td>{{ qty }}</td></tr>
+                <tr><td>{{ ean }}</td></tr>
+            {% endfor %}
+        </table> 
+        <table border="1">
+            <tr><th>Quantity</th></tr>
+            {% for ean, qty in results %}
+                <tr><td>{{ qty }}</td></tr>
             {% endfor %}
         </table>
+        </div>
+     <footer >
+    &copy Igor
+    </footer>
+
     {% endif %}
 '''
 
@@ -28,7 +120,7 @@ def find_ean_and_quantity(text):
     # Шаблон для поиска EAN (13-значный штрих-код)
     ean_pattern = r'\b\d{13}\b'
     # Шаблон для поиска однозначного количества (одно число)
-    quantity_pattern = r'\b\d\b'
+    quantity_pattern = r'\b\d{1,2}\b'
 
     # Поиск всех EAN кодов и количеств
     ean_codes = re.findall(ean_pattern, text)
@@ -48,6 +140,7 @@ def index():
         text = request.form['text']
         results = find_ean_and_quantity(text)
     return render_template_string(html_template, results=results)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
